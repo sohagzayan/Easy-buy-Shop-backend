138a3b561b8970dac4a2c4206a4028a980848846
sohagzayan <gdsohag360@gmail.com>
2022-10-17 23:26:37 -0700

    re-name index js file name

--- index.js
+++ index.js
@@ -1,56 +1,15 @@
-/** External Require */
-const createError = require("http-errors");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
+const colors = require("colors");
+const connectionDb = require("./connection/connectionDB");
+const dotenv = require("dotenv").config();
 
-/** Internal Require */
-const userLoginRoute = require("./routes/user.login");
-const userSigninRoute = require("./routes/user.signin");
-const userRoute = require("./routes/user.route");
-const toolsRoutes = require("./routes/tools.route");
-const reviewsRoutes = require("./routes/review.route");
-const purchaseRoutes = require("./routes/purchase.route");
-const paymetnRoutes = require("./routes/payment.route");
-const bookmarkRoute = require("./routes/bookmarkRoute.route");
-const addToCardRoute = require("./routes/addToCard.route");
-const addShopReview = require("./routes/add_Shop_review_route");
-const {
-  NotFindRouteHandaler,
-  globalErrorHandaler,
-} = require("./middlWare/ErrorHandaling.middlewere");
+const app = require("./index");
+
+/** Database connection */
+connectionDb();
+
+/** Server  */
 const port = process.env.PORT || 5000;
 
-/* meddleWere */
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("hello word");
+app.listen(port, () => {
+  console.log(`server is running on ${port}`.blue);
 });
-
-/* User Route */
-app.use("/api/v1/user/login", userLoginRoute);
-app.use("/api/v1/user/signin", userSigninRoute);
-app.use("/api/v1/user/user", userRoute);
-/** Tools Route */
-app.use("/api/v1/tools", toolsRoutes);
-/** Reviews Routes */
-app.use("/api/v1/review", reviewsRoutes);
-/** Purchase Routes */
-app.use("/api/v1/purchase", purchaseRoutes);
-/** Payment Routes */
-app.use("/api/v1/payment", paymetnRoutes);
-/** Bookmark  Routes */
-app.use("/api/v1/bookmark", bookmarkRoute);
-app.use("/api/v1/addToCard", addToCardRoute);
-/** Shop Review */
-app.use("/api/v1/shopReview", addShopReview);
-/** Not Find Routing */
-app.use(NotFindRouteHandaler);
-/** Global Error Handler */
-app.use(globalErrorHandaler);
-
-module.exports = app;
