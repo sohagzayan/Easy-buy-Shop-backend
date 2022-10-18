20aec5213f38b584fafa0109807afbdff0e1d057
sohagzayan <gdsohag360@gmail.com>
2022-10-18 01:23:20 -0700

    add cors policiry base ur

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const port = process.env.PORT || 5000;
 
 /* meddleWere */
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://easy-buy-shop.vercel.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
